26f305230d302b232e9b2fb0a87f348a6094cead
samyoshon <samuelyshon@gmail.com>
2015-11-12 12:13:40 -0800

    job description labels

--- js/controllers/jobsController.js
+++ js/controllers/jobsController.js
@@ -14,6 +14,18 @@ function JobsController ($http, jobsFactory, $timeout){
 	self.jobApply = jobApply;
 
 	getJobs();
+	// maxLength();
+	
+	// function maxLength() {
+	// 	var text_max = 700;
+	// 	$('#textarea_feedback').html(text_max + ' characters remaining');
+
+	// 	$('#textarea').keyup(function() {
+	// 		var text_length = $('#textarea').val().length;
+	// 		var text_remaining = text_max - text_length;
+	// 		$('#textarea_feedback').html(text_remaining + ' characters remaining');
+	// 	});
+	// }
 
 	//SET UP $HTTP REQUEST TO GET ALL JOBS //
 	function getJobs(){
